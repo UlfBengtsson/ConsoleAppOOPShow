af87dd449f8774b5536499efe90d22d4d65f749c
Ulf Bengtsson <ulf.bengtsson@lexicon.se>
2020-09-28 12:52:43 +0200

    Add project files.

---
+++ ConsoleAppOOPShow/Car.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppOOPShow
+{
+    class Car
+    {
+        //static
+        private static int counter = 0;
+        public static int Counter { get { return counter; } }
+
+        //fields
+        int id;
+        int modelYear;
+        string brand;
+        string modelName;
+        public string color;// not following ecapsilatuon
+
+        string regPlate;
+        public string RegPlate
+        {
+            get { return regPlate; }
+            set { regPlate = value.ToUpper(); }
+        }
+
+        //constructor
+        public Car(int modelYear, string brand, string carModelName)
+        {
+            id = ++counter;
+
+            this.modelYear = modelYear;
+            this.brand = brand;
+            modelName = carModelName;
+        }
+
+        public Car(int modelYear, string brand, string carModelName, string color, string regPlate)
+            : this(modelYear, brand, carModelName)
+        {
+            this.color = color;
+            RegPlate = regPlate;
+        }
+
+
+        public string CarInfomation()
+        {
+            return $"Car id:{id}\nBrand: {brand}\nModel: {modelName}\nYear: {modelYear}\nReg. Plate: {regPlate}\nColor: {color}\n\n";
+        }
+    }
+}
---
+++ ConsoleAppOOPShow/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleAppOOPShow
+{
+    // defualt accsses is Private
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Car> cars = new List<Car>();
+
+            Console.WriteLine("Amount of cars: " + Car.Counter);
+
+            //Car myCar = new Car(2000, "BMW", "525i");
+
+            //myCar.color = "Blue";
+
+            //myCar.RegPlate = "abc123";
+
+            cars.Add(new Car(2000, "BMW", "525i", "Blue","abc123"));
+
+            Console.WriteLine("Amount of cars: " + Car.Counter);
+            //Console.WriteLine(myCar.CarInfomation());
+
+            //Car odin = new Car(2012, "Volvo", "XC90");
+
+            //odin.color = "Gray";
+            //odin.RegPlate = "QWe098";
+
+            cars.Add(new Car(2012, "Volvo", "XC90", "Gray", "QWe098"));
+
+            Console.WriteLine("Amount of cars: " + Car.Counter);
+            //Console.WriteLine(odin.CarInfomation());
+
+            //Car erik = new Car(2008, "Opel", "Astra", "Yellow", "zxc456");
+
+            //Console.WriteLine(erik.CarInfomation());
+
+            cars.Add(new Car(2008, "Opel", "Astra", "Yellow", "zxc456"));
+
+            Console.WriteLine("Amount of cars: " + Car.Counter);
+
+            foreach (Car aCar in cars)
+            {
+                Console.WriteLine(aCar.CarInfomation());
+            }
+        }
+    }
+}
