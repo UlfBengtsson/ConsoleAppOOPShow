ecf8eb4d4b2011c0326d33b8f63b3fc75f346018
Ulf Bengtsson <ulf.bengtsson@lexicon.se>
2020-09-30 11:49:09 +0200

    added class

---
+++ ConsoleAppOOPShow/Garage.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppOOPShow
+{
+    class Garage
+    {
+        public Car ParkingSlot { get; set; }
+    }
+}
